194c48a6a7d36d406d1cac0638e69b6557bdf07b
mingerzeng <www.mingerzeng@gmail.com>
2022-04-16 13:10:56 +0800

    「商品功能 - 實作查詢商品列表（基礎實作）」

--- src/main/java/com/kitezeng/springbootmall/dao/ProductDao.java
+++ src/main/java/com/kitezeng/springbootmall/dao/ProductDao.java
@@ -3,7 +3,12 @@ package com.kitezeng.springbootmall.dao;
 import com.kitezeng.springbootmall.dto.ProductRequest;
 import com.kitezeng.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
